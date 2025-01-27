9bba135b92d8f084cc4f1222122c21072c5f3778
DogeCN <hark2009lbf@outlook.com>
2025-01-28 03:04:20 +0800

    Update

--- sprites.py
+++ sprites.py
@@ -10,37 +10,29 @@ class ChessCell(AnimatedTrapezoid):
             (B_TOPLEFT[0] + c * C_SIZE[1], B_TOPLEFT[1] + r * C_SIZE[0]),
             C_SIZE,
         )
-
-
-class ChessPiece(sprite.Sprite):
-    def __init__(self, surface: Surface, c, r):
-        super().__init__()
         self.clicked = False
-        self.surface = surface
-        self.frame = ChessCell(surface, c, r)
-        self.rect = self.frame.copy()
 
     def update(self):
-        self.frame.update()
+        super().update()
         pressed = mouse.get_pressed()
-        if True in pressed and self.rect.collidepoint(mouse.get_pos()):
+        if True in pressed and self.collidepoint(mouse.get_pos()):
             self.clicked = not pressed.index(True)
 
     def draw(self):
         pos = mouse.get_pos()
-        if self.rect.collidepoint(pos):
-            self.frame.draw(YELLOW)
+        if self.collidepoint(pos):
+            super().draw(YELLOW)
         elif self.clicked:
-            self.frame.draw(GREEN)
+            super().draw(GREEN)
         else:
-            self.frame.draw(GRAY)
+            super().draw(GRAY)
 
 
 class Board(AnimatedTrapezoid):
     def __init__(self, surface):
         super().__init__(surface, B_TOPLEFT, B_SIZE)
         self.frames = [
-            [ChessPiece(surface, c, r) for c in range(COLUMN)] for r in range(ROW)
+            [ChessCell(surface, c, r) for c in range(COLUMN)] for r in range(ROW)
         ]
 
     def update(self):
